114e4cd63658cccbfce1e475e9bfb1a021384379
Leonardo Cordeiro <leonardo.cordeiro@caelum.com.br>
2015-05-06 02:42:34 -0300

    alteracoes

--- app/src/main/java/net/leonardocordeiro/testerecycleviewecardview/MainActivity.java
+++ app/src/main/java/net/leonardocordeiro/testerecycleviewecardview/MainActivity.java
@@ -1,39 +1,33 @@
 package net.leonardocordeiro.testerecycleviewecardview;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-
 public class MainActivity extends ActionBarActivity {
+
+    private Toolbar mToolbar;
+    private Toolbar mToolbarBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_main);
+
+        mToolbar.setNavigationIcon(R.drawable.ic_menu);
+        mToolbar.setTitle("UEZO");
+
+        setSupportActionBar(mToolbar);
+
+        mToolbarBottom = (Toolbar) findViewById(R.id.inc_toolbar_bottom);
+        mToolbarBottom.inflateMenu(R.menu.toolbar_bottom_menu);
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
